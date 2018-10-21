b38c7f3806fb653ec1ebdc74da17440c3940a984
crin <crinuyi@gmail.com>
2018-10-21 14:33:10 +0200

    dodanie ekranu startowego (pierwszego)

--- src/main/java/Application/Main.java
+++ src/main/java/Application/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../FXML/MainMenu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../../resources/FXML/Login.fxml"));
         primaryStage.setTitle("Purranya");
         primaryStage.setScene(new Scene(root, 1024, 768));
         primaryStage.show();
