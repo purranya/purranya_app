ad423c7e0b0a662b1b4e7d7a260e20ead7eff504
crin <crinuyi@gmail.com>
2018-10-24 20:29:58 +0200

    dodano JFoenix to Mavena + kosmetyczne poprawki nazw pakietów

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,4 +1,4 @@
-package main.java.Application;
+package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,11 +6,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.net.URL;
+
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../../resources/FXML/Login.fxml"));
+        URL url = new File("src/main/resources/fxml/Login.fxml").toURL();
+        Parent root = FXMLLoader.load(url);
         primaryStage.setTitle("Purranya");
         primaryStage.setScene(new Scene(root, 1024, 768));
         primaryStage.show();
