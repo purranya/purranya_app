993a6a6ec78d5e9da5ab8569d3374d7395d94d6b
crin <crinuyi@gmail.com>
2018-11-18 00:42:47 +0100

    Dodanie nagłówka w kalendarzu (z miesiącem)

--- src/main/java/controllers/Calendar.java
+++ src/main/java/controllers/Calendar.java
@@ -1,16 +1,73 @@
 package controllers;
 
 import com.jfoenix.controls.JFXButton;
-import controllers.Login;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
+import org.joda.time.*;
 
-public class Calendar {
-    @FXML private GridPane calendarGrid;
-    @FXML private Text headerOfCalendar;
-    @FXML private JFXButton previous;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class Calendar implements Initializable {
     @FXML private JFXButton next;
+    @FXML private JFXButton previous;
+    @FXML private Text headerOfCalendar;
+    @FXML private GridPane calendarGrid;
 
+    List<BorderPane> day = new ArrayList<>(42);
 
+    private void createCalendar(DateTime dt) {
+        String month = "";
+        switch(dt.monthOfYear().getAsString()) {
+            case "1":
+                month = "Styczeń";
+                break;
+            case "2":
+                month = "Luty";
+                break;
+            case "3":
+                month = "Marzec";
+                break;
+            case "4":
+                month = "Kwiecień";
+                break;
+            case "5":
+                month = "Maj";
+                break;
+            case "6":
+                month = "Czerwiec";
+                break;
+            case "7":
+                month = "Lipiec";
+                break;
+            case "8":
+                month = "Sierpień";
+                break;
+            case "9":
+                month = "Wrzesień";
+                break;
+            case "10":
+                month = "Październik";
+                break;
+            case "11":
+                month = "Listopad";
+                break;
+            case "12":
+                month = "Grudzień";
+                break;
+        }
+        headerOfCalendar.setText(month + " " + dt.getYear());
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        DateTime dt = DateTime.now();
+        createCalendar(dt);
+    }
 }
