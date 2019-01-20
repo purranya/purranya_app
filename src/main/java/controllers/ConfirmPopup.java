3b5447f58fc89a69eff27f9b82d879f0c5141665
crin <crinuyi@gmail.com>
2019-01-20 17:47:41 +0100

    edited combobox css and added confirm popup

---
+++ src/main/java/controllers/ConfirmPopup.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class ConfirmPopup {
+    @FXML private Text popupText;
+    @FXML private JFXButton yes;
+    @FXML private JFXButton no;
+
+    @FXML
+    void cancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void confirm(ActionEvent event) {
+
+    }
+
+}
