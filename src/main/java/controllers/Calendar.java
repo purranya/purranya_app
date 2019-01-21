1bd250ec74b2ea420be0aae2f1059dc35c36c071
malinowskikam <kamiltruskawa@gmail.com>
2019-01-21 16:48:04 +0100

    calendar generation v1

--- src/main/java/controllers/Calendar.java
+++ src/main/java/controllers/Calendar.java
@@ -1,10 +1,20 @@
 package controllers;
 
+import application.App;
+import data_util.DateUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import org.joda.time.DateTime;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Calendar {
+public class Calendar implements Initializable {
+
+    private static DateTime currentMonth;
 
     @FXML
     private Text header;
@@ -13,38 +23,84 @@ public class Calendar {
     private Text date;
 
     @FXML
-    void addEvent(ActionEvent event) {
+    private GridPane days;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        currentMonth = new DateTime(DateTime.now().year().get(), DateTime.now().monthOfYear().get(),1,0,0,0);
+        createCalendar();
     }
 
+    public void createCalendar()
+    {
+        header.setText(DateUtils.getMonthName(currentMonth.monthOfYear().get()) + " " + currentMonth.year().get());
+        date.setText(DateTime.now().toString("dd MMMM YYYY"));
+
+        //getting first monday
+        DateTime day = currentMonth.minusDays(currentMonth.dayOfWeek().get()+1);
+
+        for(int week_i=0;week_i<6;week_i++)
+        {
+            for(int day_i=0;day_i<7;day_i++)
+            {
+                Text dayLabel = new Text(Integer.toString(day.getDayOfMonth()));
+
+                VBox dayField = new VBox(dayLabel);
+                days.add(dayField,day_i,week_i);
+
+                day = day.plusDays(1);
+            }
+        }
+    }
+
+    /** (button) przejście do popupu umożliwiającej dodanie wydarzenia */
     @FXML
-    void cancel(ActionEvent event) {
-
+    void addEvent(ActionEvent event) {
+        EventPopup.display();
+        App.primaryStageManager.reloadScene("Calendar");
     }
 
+    /** (button) usunięcie bieżącego kalendarza */
     @FXML
     void delete(ActionEvent event) {
-
+        boolean answer = ConfirmPopup.display("Czy na pewno chcesz usunąć kalendarz\n \"" + App.calendarManager.getCalendarName() + "\"?");
+        if(answer) {
+            App.calendarManager.deleteCalendar();
+            App.primaryStageManager.reloadScene("MainMenu");
+        }
     }
 
+    /** edycja bieżącego kalendarza
+     * TODO wczytywanie informacji z bieżącego kalendarza (może w konstruktorze */
     @FXML
     void edit(ActionEvent event) {
-
+        CalendarPopup.displayEdit();
     }
 
+    /** (button) wyjście z kalendarza */
+    @FXML
+    void cancel(ActionEvent event) {
+        App.primaryStageManager.reloadScene("MainMenu");
+    }
+
+    /** (button) przejście do sceny zarządzającej etykietami */
     @FXML
     void manageLabels(ActionEvent event) {
+        App.primaryStageManager.reloadScene("LabelManager");
 
     }
 
+    /** (button) wyświetlenie miesiąca następnego niż na bieżącej scenie */
     @FXML
     void nextMonth(ActionEvent event) {
-
+        currentMonth = currentMonth.plusMonths(1);
+        createCalendar();
     }
 
+    /** (button) wyświetlenie miesiąca poprzedniego niż na bieżącej scenie */
     @FXML
     void previousMonth(ActionEvent event) {
-
+        currentMonth = currentMonth.minusMonths(1);
+        createCalendar();
     }
-
 }
--- src/main/java/data_util/DateUtils.java
+++ src/main/java/data_util/DateUtils.java
@@ -23,4 +23,36 @@ public class DateUtils {
         DateTime dateTimeUtc = dateTime.withZone(DateTimeZone.UTC);
         return LocalDate.of(dateTimeUtc.getYear(), dateTimeUtc.getMonthOfYear(), dateTimeUtc.getDayOfMonth());
     }
+
+    public static String getMonthName(int dayOfMonth)
+    {
+        switch(dayOfMonth) {
+            case 1:
+                return "Styczeń";
+            case 2:
+                return "Luty";
+            case 3:
+                return "Marzec";
+            case 4:
+                return "Kwiecień";
+            case 5:
+                return "Maj";
+            case 6:
+                return "Czerwiec";
+            case 7:
+                return "Lipiec";
+            case 8:
+                return "Sierpień";
+            case 9:
+                return "Wrzesień";
+            case 10:
+                return "Październik";
+            case 11:
+                return "Listopad";
+            case 12:
+                return "Grudzień";
+            default:
+                return null;
+        }
+    }
 }
